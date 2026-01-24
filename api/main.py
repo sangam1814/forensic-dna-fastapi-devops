470244989e0e2aa36d0bf4ed68ab9cce72d96249
Sangam Raj <sangam@Sangams-MacBook-Air.local>
2026-01-24 11:56:15 +0530

    add CD pipeline

--- api/main.py
+++ api/main.py
@@ -2,12 +2,11 @@ from fastapi import FastAPI
 from .routers import populations, loci, profiles, evidence, matches, auth
 from api.routers.health import router as health_router
 
-
 app = FastAPI(title="Privacy-Aware Forensic DNA API")
 
-# ✅ register all routers
-app.include_router(health_router)      # 🩺 HEALTHCHECK
-app.include_router(auth.router)        # 🔑 LOGIN / JWT
+# register all routers
+app.include_router(health_router)        # HEALTHCHECK
+app.include_router(auth.router)           # LOGIN / JWT
 app.include_router(populations.router)
 app.include_router(loci.router)
 app.include_router(profiles.router)
